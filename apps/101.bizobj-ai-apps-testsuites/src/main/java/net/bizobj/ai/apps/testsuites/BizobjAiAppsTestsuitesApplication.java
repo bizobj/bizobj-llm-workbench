216be77be70d1376d0a4430eac4ac81e9b701425
thinkbase <thinkbase.net@gmail.com>
2025-06-01 20:47:10 +0800

    The init

---
+++ apps/101.bizobj-ai-apps-testsuites/src/main/java/net/bizobj/ai/apps/testsuites/BizobjAiAppsTestsuitesApplication.java
@@ -0,0 +1,13 @@
+package net.bizobj.ai.apps.testsuites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizobjAiAppsTestsuitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizobjAiAppsTestsuitesApplication.class, args);
+	}
+
+}
---
+++ apps/101.bizobj-ai-apps-testsuites/src/test/java/net/bizobj/ai/apps/testsuites/BizobjAiAppsTestsuitesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bizobj.ai.apps.testsuites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizobjAiAppsTestsuitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
